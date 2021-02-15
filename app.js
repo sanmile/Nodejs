b4048eb04300a9c0d2aa3a6198166934dc35f29b
Sandra Moron Oñate <Sandra.M.Moron.Onate@co.ey.com>
2021-02-14 20:36:13 -0500

    add some css

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
@@ -7,12 +8,13 @@ const shopRoute = require('./routes/shop');
 
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 
 router.get('/add-product',(req, res, next)=>{
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"></input><button type="submit">New product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
 });
     
 router.post('/add-product',(req, res, next)=>{
