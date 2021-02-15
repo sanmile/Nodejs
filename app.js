a2c538e07bb5f4710816f3a2ecebdda9e3eb9d00
Sandra Moron Oñate <Sandra.M.Moron.Onate@co.ey.com>
2021-02-14 19:15:04 -0500

    use express and routing

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 
-console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/admin', adminRoute);
+app.use(shopRoute);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
+app.listen(3000);
